9d33febb33aa265afde8164cb11ca59ec2aa1333
Sinan Ulusan <sinanulusan99@gmail.com>
2025-09-16 19:33:36 +0300

    Initial commit

---
+++ apps/call-reservation-tool/jest.config.ts
@@ -0,0 +1,10 @@
+export default {
+  displayName: 'call-reservation-tool',
+  preset: '../../jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/call-reservation-tool',
+};
