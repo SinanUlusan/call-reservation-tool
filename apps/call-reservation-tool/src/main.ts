1c126fc9fc96ac5032324adedf6a32e08463753f
Sinan Ulusan <sinanulusan99@gmail.com>
2025-09-22 15:33:06 +0300

    netlify cors integration completed

--- apps/call-reservation-tool/src/main.ts
+++ apps/call-reservation-tool/src/main.ts
@@ -30,6 +30,7 @@ async function bootstrap() {
             'http://localhost:3000',
             'http://localhost:3001',
             'https://calenmate.vercel.app',
+            'https://calenmate.netlify.app',
             'https://calenmate-bm6mgvm32-sinanulusans-projects.vercel.app',
             'https://calenmate-git-main-sinanulusans-projects.vercel.app',
             'https://calenmate-sinanulusans-projects.vercel.app',
